fixed weight scaling.

sgd/src/dirty_image.py
@@ -1,21 +1,18 @@
-import mpol
 from mpol import coordinates, gridding
-import numpy as np
 import matplotlib.pyplot as plt
-from src import loaddata
+import load_data
 import argparse
 
-def init_dirty_imager(filename):
-    uu, vv, data, weight = loaddata.get_basic_data(filename)
-
-    coords = coordinates.GridCoords(cell_size=0.005, npix=800)
-    return gridding.DirtyImager(
+def init_dirty_imager():
+    vis_data = load_data.vis_data
+    
+    coords = coordinates.GridCoords(cell_size=0.005, npix=1028)
+    return gridding.DirtyImager.from_tensors(
         coords=coords,
-        uu=uu,
-        vv=vv,
-        weight=weight,
-        data_re=np.real(data),
-        data_im=np.imag(data),
+        uu=vis_data.uu,
+        vv=vis_data.vv,
+        weight=vis_data.weight,
+        data=vis_data.data,
     )
 
 def main():
@@ -23,14 +20,12 @@ def main():
     parser = argparse.ArgumentParser(
         description="Make a dirty image with the visibilities."
     )
-    parser.add_argument("file", help="Path to asdf file")
-    parser.add_argument("outfile", help="Output file") 
+    parser.add_argument("outfile", help="Output image") 
     args = parser.parse_args()
     
-    imager = init_dirty_imager(args.file)
+    imager = init_dirty_imager()
 
     img, beam = imager.get_dirty_image(weighting="briggs", robust=0.0)
-
 
     # set plot dimensions
     xx = 8 # in

sgd/src/load_data.py
@@ -5,6 +5,11 @@ import visread.process
 from mpol import coordinates, fourier, utils
 from pathlib import Path
 
+from numpy import typing as npt
+from typing import Any
+
+from dataclasses import dataclass
+
 # assumes you're running this from sgd/, not sgd/src/
 _npz_path = Path("data/mock_data.npz")
 
@@ -29,12 +34,23 @@ uu_lam, vv_lam = visread.process.broadcast_and_convert_baselines(
 )
 
 # stored as 1D, broadcast to (nchan, nvis)
-weight = np.broadcast_to(np.float64(archive["weight"]), uu_lam.shape)
+w = np.float64(archive["weight"])
+
+# because we took only a fraction of the data, the original weight produces very 
+# noisey visibilities.
+# We will reduce sigma by a factor s
+s = 10.0
+# which increases weight by a factor s^2
+w *= s**2
+weight = np.broadcast_to(w, uu_lam.shape)
 
 # set up image and fourier coordinates
 coords = coordinates.GridCoords(cell_size=_cell_size, npix=npix)
 
-# fake data routine expects tensors
+# set random seed to make dataset repeatable
+torch.manual_seed(42)
+
+# fake data routine expects tensors and returns a tensor
 data, _ = fourier.generate_fake_data(
     packed_cube,
     coords,
@@ -43,15 +59,18 @@ data, _ = fourier.generate_fake_data(
     torch.from_numpy(weight.copy()),
 )
 
-# package everything up as a dataclass for easy use in other routines
+# package everything up for easy use in other routines using dataclasses
+@dataclass
+class VisData:
+    uu: torch.Tensor
+    vv: torch.Tensor
+    weight: torch.Tensor
+    data: torch.Tensor
 
 
-# @pytest.fixture(scope="session")
-# def mock_dataset_np(baselines_2D_np, weight_2D_t, mock_data_t):
-#     uu, vv = baselines_2D_np
-#     weight = utils.torch2npy(weight_2D_t)
-#     data = utils.torch2npy(mock_data_t)
-#     data_re = np.real(data)
-#     data_im = np.imag(data)
-
-#     return (uu, vv, weight, data_re, data_im)
+vis_data = VisData(
+    torch.from_numpy(uu_lam),
+    torch.from_numpy(vv_lam),
+    torch.from_numpy(weight.copy()),
+    data,
+)

sgd/src/plot_baselines.py
@@ -1,26 +1,31 @@
 import matplotlib.pyplot as plt 
 import numpy as np
 import argparse
-from src import loaddata
+import load_data
+
+from mpol import utils
 
 def main():
     parser = argparse.ArgumentParser(
         description="Plot the visibilities."
     )
-    parser.add_argument("file", help="Path to asdf file")
+
     parser.add_argument("outfile", help="Output file") 
     args = parser.parse_args()
     
-    uu, vv, data, weight = loaddata.get_basic_data(args.file)
+    vis_data = load_data.vis_data
     
+    uu = utils.torch2npy(vis_data.uu)
+    vv = utils.torch2npy(vis_data.vv)
+
     # augment to include complex conjugates
     uu = np.concatenate([uu, -uu])
     vv = np.concatenate([vv, -vv])
 
     fig, ax = plt.subplots(nrows=1, figsize=(6,6))
     ax.scatter(uu, vv, s=1.5, rasterized=True, linewidths=0.0, c="k")
-    ax.set_xlabel(r"$u$ [k$\lambda$]")
-    ax.set_ylabel(r"$v$ [k$\lambda$]")
+    ax.set_xlabel(r"$u$ [$\lambda$]")
+    ax.set_ylabel(r"$v$ [$\lambda$]")
     ax.axis('equal')
     fig.subplots_adjust(left=0.15, right=0.85)
     fig.savefig(args.outfile, dpi=300)