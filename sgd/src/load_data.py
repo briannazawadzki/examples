b0cbb0d3700d0d539dbf303ccc334f66640ac1bd
Ian Czekala <iancze@gmail.com>
2024-02-29 15:05:19 +0000

    made dataloading float32

--- sgd/src/load_data.py
+++ sgd/src/load_data.py
@@ -18,7 +18,7 @@ _nchan = 1
 _cell_size = 0.005
 
 archive = np.load(_npz_path)
-img2D_butterfly = np.float64(archive["img"])
+img2D_butterfly = archive["img"]
 npix, _ = img2D_butterfly.shape
 
 sky_cube = torch.unsqueeze(torch.from_numpy(img2D_butterfly), dim=0)
@@ -28,13 +28,13 @@ packed_cube = utils.sky_cube_to_packed_cube(sky_cube)
 
 # stored as (nvis,)
 # broadcast to (nchan, nvis)
-uu_m, vv_m = np.float64(archive["uu"]), np.float64(archive["vv"])
+uu_m, vv_m = archive["uu"], archive["vv"]
 uu_lam, vv_lam = visread.process.broadcast_and_convert_baselines(
     uu_m, vv_m, np.array([230.0e9])
 )
 
 # stored as 1D, broadcast to (nchan, nvis)
-w = np.float64(archive["weight"])
+w = archive["weight"]
 
 # because we took only a fraction of the data, the original weight produces very 
 # noisey visibilities.
@@ -50,6 +50,11 @@ coords = coordinates.GridCoords(cell_size=_cell_size, npix=npix)
 # set random seed to make dataset repeatable
 torch.manual_seed(42)
 
+# convert to float32
+uu_lam = np.float32(uu_lam)
+vv_lam = np.float32(vv_lam)
+weight = np.float32(weight)
+
 # fake data routine expects tensors and returns a tensor
 data, _ = fourier.generate_fake_data(
     packed_cube,
@@ -58,6 +63,7 @@ data, _ = fourier.generate_fake_data(
     torch.as_tensor(vv_lam),
     torch.from_numpy(weight.copy()),
 )
+
 
 # package everything up for easy use in other routines using dataclasses
 @dataclass
